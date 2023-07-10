b0c23a9a02af70c1aef15ffa76aebac2eab4a55e
alamin2811 <alaminbinmonsur@gmail.com>
2023-07-11 01:11:19 +0600

    modified

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    $('#userMenuBtn').click(function () {
+        $('#sideMenu').addClass('active');
+        $('#sideMenu').removeClass('hide');
+        $('.user-body-section').addClass('active');
+    });
+    $('#sideMenuCloseBtn').click(function () {
+        $('#sideMenu').toggleClass('hide');
+        $('#sideMenu').removeClass('active');
+        $('.user-body-section').removeClass('active');
+    });
+});
+$(document).ready(function () {
+    $('#complete-payment-btn').click(function () {
+        $('.payment-modal-content1').hide();
+        $('.payment-modal-content2').show();
+    });
+    $('#confirm-payment-btn').click(function () {
+        $('.payment-modal-content2').hide();
+        $('.payment-modal-content3').show();
+    });
+    $('.renew-btn').click(function () {
+        $('.payment-modal-content2').hide();
+        $('.payment-modal-content3').hide();
+        $('.payment-modal-content1').show();
+    });
+});
+
+// $(document).ready(function() {
+//     $('div').click(function() {
+//       $('div').removeClass('active'); // Remove the "active" class from all divs
+//       $(this).addClass('active'); // Add the "active" class to the clicked div
+//     });
+//   });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var countElement = document.getElementById('count');
+    var incrementButton = document.getElementById('increment');
+    var decrementButton = document.getElementById('decrement');
+
+    var count = 0;
+
+    // Increment the count when the "+" button is clicked
+    incrementButton.addEventListener('click', function () {
+        count++;
+        countElement.textContent = count;
+    });
+
+    // Decrement the count when the "-" button is clicked
+    decrementButton.addEventListener('click', function () {
+        if (count > 0) {
+            count--;
+            countElement.textContent = count;
+        }
+    });
+});
+
+
+
